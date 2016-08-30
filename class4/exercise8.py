977b1b9c168b50dd3db1327164e277e731cb22ca
Earl Aboytes <eaboytes@gmail.com>
2016-08-29 22:09:18 -0700

    updating exercise 8 week 4

--- class4/exercise8.py
+++ class4/exercise8.py
@@ -3,7 +3,7 @@ __author__ = 'eaboytes'
 
 
 from netmiko import ConnectHandler
-from devices import pynet2
+from devices import pynet1, pynet2
 from getpass import getpass
 
 
@@ -11,26 +11,28 @@ from getpass import getpass
  Use Netmiko to change the logging buffer size (logging buffered <size>) and to disable console logging (no logging console) from a file on both pynet-rtr1 and pynet-rtr2 
 '''
 
-username = raw_input('Username:')
+#username = raw_input('Username:')
 password = getpass()
 ver = 'sh run | inc logging buffered'
-device = pynet2
+for dvc_dict in (pynet1, pynet2):
+    dvc_dict['password'] = password
 
 def main():
     '''
     Logs into each router and executes commands from file config_file.txt. Verification of config change. 
     '''
     print '=' *80
-    ssh_conn = ConnectHandler(**device)
-    output = ssh_conn.config_mode()
-    print output
-    output = ssh_conn.send_config_from_file(config_file='config_file.txt')
-    print output
-    output = prompt = ssh_conn.find_prompt()
-    output = ssh_conn.send_command(ver)
-    print '\n %s from %s \n %s' %(ver, prompt, output)
-    print
-    print '=' *80
+    for device in (pynet1, pynet2):
+        ssh_conn = ConnectHandler(**device)
+        output = ssh_conn.config_mode()
+        print output
+        output = ssh_conn.send_config_from_file(config_file='config_file.txt')
+        print output
+        output = prompt = ssh_conn.find_prompt()
+        output = ssh_conn.send_command(ver)
+        print '\n %s from %s \n %s' %(ver, prompt, output)
+        print
+        print '=' *80
 
 if __name__=="__main__":
     main()
