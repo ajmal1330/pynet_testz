ea551a654eaa6636b1dba01a6081ee054f3359c8
Earl Aboytes <eaboytes@gmail.com>
2016-08-29 21:48:44 -0700

    adding exercise 8 week 4

--- class4/exercise8.py
+++ class4/exercise8.py
@@ -3,80 +3,16 @@ __author__ = 'eaboytes'
 
 
 from netmiko import ConnectHandler
+from devices import pynet2
+from getpass import getpass
+
 
 '''
  Use Netmiko to change the logging buffer size (logging buffered <size>) and to disable console logging (no logging console) from a file on both pynet-rtr1 and pynet-rtr2 
 '''
 
-#(Cisco 881)
-pynet1 = {
-    'device_type' : 'cisco_ios',
-    'ip' : '184.105.247.70',
-    #'snmp_port' : '161',
-    'port' : '22',
-    'username' : 'pyclass',
-    'password' : '88newclass',
-}
-
-#(Cisco 881)
-pynet2 = {
-    'device_type': 'cisco_ios',
-    'ip' : '184.105.247.71',
-    #'snmp_port' : '161',
-    'port' : '22',
-    'username' : 'pyclass',
-    'password' : '88newclass',
-}
-
-#(Arista vEOS switch)
-pynetsw1  = {
-    'device_type' : 'Arista',
-    'ip' :'184.105.247.72',
-    'ssh_port' : '22',
-    'eapi_port' : '443',
-    'username' : 'admin1',
-    'password' : '99saturday',
-}
-
-#(Arista vEOS switch)
-pynetsw2 = {
-    'device_type' : 'Arista',
-    'ip' : '184.105.247.73',
-    'ssh_port' : '22',
-    'eapi_port' : '443',
-    'username' : 'admin1',
-    'password' : '99saturday',
-}
-# (Arista vEOS switch)
-pynetsw3 = {
-    'device_type' : 'Arista',
-    'ip' : '184.105.247.74',
-    'ssh_port' : '22',
-    'eapi_port' : '443',
-    'username' : 'admin1',
-    'password' : '99saturday',
-}
-#(Arista vEOS switch)
-pynetsw4 = {
-    'device_type' : 'Arista',
-    'ip' : '184.105.247.75',
-    'ssh_port' : '22',
-    'eapi_port' : '443',
-    'username' : 'admin1',
-    'password' : '99saturday',
-}
-
-#(Juniper SRX)
-pynetsrx1 = {
-    'device_type' : 'juniper',
-    'ip' : '184.105.247.76',
-    'port' : '22',
-    'username' : 'pyclass',
-    'password' : '88newclass',
-    'secret' : '',
-}
-
-
+username = raw_input('Username:')
+password = getpass()
 ver = 'sh run | inc logging buffered'
 device = pynet2
 
