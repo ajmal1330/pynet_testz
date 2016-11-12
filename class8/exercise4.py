a00b61bfccd8a42680063b62332a5795961ce358
eaboytes <eaboytes@gmail.com>
2016-11-12 12:11:13 -0800

    correction ex4

--- class8/exercise4.py
+++ class8/exercise4.py
@@ -5,8 +5,8 @@ def main():
     device1 = NetworkDevice.objects.get(device_name='pynet-tst1')
     device2 = NetworkDevice.objects.get(device_name='pynet-tst2')
     all_devices = NetworkDevice.objects.all()
-    device1.delete
-    device2.delete
+    device1.delete()
+    device2.delete()
     print all_devices
 if __name__ == "__main__":
     main()
