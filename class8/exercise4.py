3a36cf9b0c35b8c2447f55aa70976f1eb2b4da83
eaboytes <eaboytes@gmail.com>
2016-11-12 12:16:33 -0800

    correction ex4

--- class8/exercise4.py
+++ class8/exercise4.py
@@ -2,11 +2,16 @@ from net_system.models import NetworkDevice
 import django
 
 def main():
+    django.setup()
+
     device1 = NetworkDevice.objects.get(device_name='pynet-tst1')
     device2 = NetworkDevice.objects.get(device_name='pynet-tst2')
+
     all_devices = NetworkDevice.objects.all()
+
     device1.delete()
     device2.delete()
+
     print all_devices
 if __name__ == "__main__":
     main()
