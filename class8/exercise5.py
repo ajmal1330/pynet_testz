1d1e040abf013ef359ca72daa25a7190bcd068da
eaboytes <eaboytes@gmail.com>
2016-11-12 15:08:45 -0800

    initial upload of ex5

---
+++ class8/exercise5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+from datetime import datetime
+from net_system.models import NetworkDevice, Credentials
+import django
+
+def show_ver(a_device)
+    creds = a_device.credentials
+    remote_conn = ConnectHandler(device_type=a_device.device_type, ip=a_device.ip, password=a_device.password,
+                                 username=a_device.username, port=a_device.port, secret='')
+    print
+    print '#' * 80
+    print remote_conn.send_command("show ver")
+    print '#' * 80
+
+def main():
+    django.setup()
+
+    start_time = datetime.now()
+
+    devices = NetworkDevice.objects.all()
+    for a_device in devices:
+        print a_device
+        show_ver(a_device)
+    elapsed_time = datetime.now() - start_time
+    print "Elapsed time: {}".format(elapsed_time)
+if __name__ == "__main__":
+    main()
