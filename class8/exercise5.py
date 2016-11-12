14ee378adcd939112f27f2c1a529ddabc82ce41c
eaboytes <eaboytes@gmail.com>
2016-11-12 15:19:57 -0800

    ex5 formatting

--- class8/exercise5.py
+++ class8/exercise5.py
@@ -9,7 +9,7 @@ def show_ver(a_device):
     creds = a_device.credentials
     remote_conn = ConnectHandler(device_type=a_device.device_type, ip=a_device.ip_address, password=creds.password,
                                  username=creds.username, port=a_device.port, secret='')
-    print
+    print a_device
     print '#' * 80
     print remote_conn.send_command("show ver")
     print '#' * 80
@@ -17,12 +17,15 @@ def show_ver(a_device):
 def main():
     django.setup()
 
+    #Creates a starting point to time entire transaction
     start_time = datetime.now()
 
+    #goes into each device in the django database and executes a "show version" command
     devices = NetworkDevice.objects.all()
     for a_device in devices:
-        print a_device
         show_ver(a_device)
+
+    #provides the difference between starting time above and end time.  In other words, how long did this take.
     elapsed_time = datetime.now() - start_time
     print "Elapsed time: {}".format(elapsed_time)
 if __name__ == "__main__":
