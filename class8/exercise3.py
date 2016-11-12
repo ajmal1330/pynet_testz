c4dc1ed52f05f4ec7c3bfe9048d8d18b6c570b97
eaboytes <eaboytes@gmail.com>
2016-11-12 11:45:37 -0800

    ex3 for class 8

---
+++ class8/exercise3.py
@@ -0,0 +1,23 @@
+from net_system.models import NetworkDevice
+import django
+
+def main():
+    django.setup()
+
+    pynet_tst1 = NetworkDevice(
+        device_name='pynet-tst1',
+        device_type='cisco_ios',
+        ip_address='1.1.1.1',
+        port=22,
+    )
+    pynet_tst1.save()
+
+    pynet_rtr2 = NetworkDevice.objects.get_or_create(
+        device_name='pynet-tst2',
+        device_type='cisco_ios',
+        ip_address='2.2.2.2',
+        port=22,
+    )
+    pynet_tst2.save()
+if __name__ == "__main__":
+    main()
