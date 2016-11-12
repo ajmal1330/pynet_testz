6590dd8a318beccefbb2530882428192f17d82d8
eaboytes <eaboytes@gmail.com>
2016-11-12 12:08:31 -0800

    exercise4 upload

---
+++ class8/exercise4.py
@@ -0,0 +1,12 @@
+from net_system.models import NetworkDevice
+import django
+
+def main():
+    device1 = NetworkDevice.objects.get(device_name='pynet-tst1')
+    device2 = NetworkDevice.objects.get(device_name='pynet-tst2')
+    all_devices = NetworkDevice.objects.all()
+    device1.delete
+    device2.delete
+    print all_devices
+if __name__ == "__main__":
+    main()
