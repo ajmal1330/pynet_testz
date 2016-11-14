2bf1f4d3f937e731218544d972b3be34be012bc0
eaboytes <eaboytes@gmail.com>
2016-11-14 02:06:35 -0800

    correcting format on ex5

--- class8/exercise5.py
+++ class8/exercise5.py
@@ -14,9 +14,14 @@ from net_system.models import NetworkDevice, Credentials
 import django
 
 def show_ver(a_device):
+    '''Handles logging into each device and doing the show command'''
     creds = a_device.credentials
-    remote_conn = ConnectHandler(device_type=a_device.device_type, ip=a_device.ip_address, password=creds.password,
-                                 username=creds.username, port=a_device.port, secret='')
+    remote_conn = ConnectHandler(device_type=a_device.device_type,
+                                 ip=a_device.ip_address,
+                                 password=creds.password,
+                                 username=creds.username,
+                                 port=a_device.port,
+                                 secret='')
     print a_device
     print '#' * 80
     print remote_conn.send_command_expect('show ver')
@@ -24,17 +29,20 @@ def show_ver(a_device):
     print '#' * 80
 
 def main():
+
+    '''Creates a starting point to time entire transaction then goes into each device in
+    the django database and executes a "show version" command provides the difference
+    between starting time above and end time.  In other words, how long did this take.
+    '''
+
     django.setup()
 
-    #Creates a starting point to time entire transaction
     start_time = datetime.now()
 
-    #goes into each device in the django database and executes a "show version" command
     devices = NetworkDevice.objects.all()
     for a_device in devices:
         show_ver(a_device)
 
-    #provides the difference between starting time above and end time.  In other words, how long did this take.
     elapsed_time = datetime.now() - start_time
     print "Elapsed time: {}".format(elapsed_time)
 
