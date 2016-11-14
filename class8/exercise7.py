8b2898d26ec3307a02148392de558a5d91c7acb4
eaboytes <eaboytes@gmail.com>
2016-11-14 01:34:45 -0800

    correcting format of ex7

--- class8/exercise7.py
+++ class8/exercise7.py
@@ -1,30 +1,35 @@
 
 '''
 6. Use threads and Netmiko to execute 'show version' on each device in the database.
-Calculate the amount of time required to do this. What is the difference in time between executing
-'show version' sequentially versus using threads?
+Calculate the amount of time required to do this. What is the difference in time
+between executing 'show version' sequentially versus using threads?
 
 7. Repeat exercise #6 except use processes.
 '''
 
-from netmiko import ConnectHandler
 from datetime import datetime
 from net_system.models import NetworkDevice, Credentials
 import django
 from multiprocessing import Process, current_process
-import time
+from netmiko import ConnectHandler
 
 
 def show_ver(a_device):
+    '''function connects to device using ORM and retrieves output from a "show version" command'''
     creds = a_device.credentials
-    remote_conn = ConnectHandler(device_type=a_device.device_type, ip=a_device.ip, password=a_device.password,
-                                 username=a_device.username, port=a_device.port, secret='')
+    remote_conn = ConnectHandler(device_type=a_device.device_type,
+                                 ip=a_device.ip,
+                                 password=creds.password,
+                                 username=creds.username,
+                                 port=a_device.port,
+                                 secret='')
     print
     print '#' * 80
     print remote_conn.send_command_expect('show ver')
     print '#' * 80
 
 def main():
+    '''uses processes to retrieve "show version" output from all devices in ORM'''
     django.setup()
     start_time = datetime.now()
     devices = NetworkDevice.objects.all()
