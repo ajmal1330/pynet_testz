0d658d3ea9dcedfb8870cb6c4560515eb18fa575
eaboytes <eaboytes@gmail.com>
2016-11-27 13:22:37 -0800

    class 9

---
+++ class9/exercise4/mytest/world.py
@@ -0,0 +1,25 @@
+
+def func1():
+    print "World"
+
+class MyStuff(object):
+
+    def __init__(self, ip, username, version):
+        self.ip = ip
+        self.username = username
+        self.version = version
+
+    def user_same(self):
+        print 'ssh {}'.format(self.ip)
+
+    def user_diff(self):
+        print 'ssh -l {} v{} {}'.format(self.username, self.version, self.ip)
+
+if __name__ == "__main__":
+    print "This is the main program of world.py"
+
+    obj=MyStuff('10.1.1.1', 'eaboytes', '2')
+    output = obj.user_same()
+    print output
+    output = obj.user_diff()
+    print output
