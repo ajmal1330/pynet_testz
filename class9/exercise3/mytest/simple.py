37ad8f296bdeae0b89a0ea5b17a12086d9121c86
eaboytes <eaboytes@gmail.com>
2016-11-27 11:28:16 -0800

    class 9

---
+++ class9/exercise3/mytest/simple.py
@@ -0,0 +1,6 @@
+1
+
+def func2():
+    print "Simple"
+if __name__ == "__main__":
+    print 'This is the main program of simple.py'
---
+++ class9/exercise3/mytest/world.py
@@ -0,0 +1,4 @@
+def func1():
+    print "World"
+if __name__ == "__main__":
+    print "This is the main program of world.py"
