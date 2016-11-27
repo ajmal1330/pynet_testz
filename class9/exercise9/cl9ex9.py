2af560928d423ddb63eb0c9247a04ed30b17e797
eaboytes <eaboytes@gmail.com>
2016-11-27 15:12:14 -0800

    class 9 ex9

--- class9/exercise9/cl9ex9.py
+++ class9/exercise9/cl9ex9.py
@@ -4,8 +4,7 @@ Write a Python script in a different directory (not the one containing mytest).
     a. Verify that you can import mytest and call the three functions func1(), func2(), and func3().
     b. Create an object that uses MyClass. Verify that you call the hello() and not_hello() methods.
 '''
-import mytest
-from mytest import *
+from mytest import func1, func2, func3, MyClass
 func1()
 func2()
 func3()
